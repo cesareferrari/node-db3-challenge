00ce65ba3847ade8398ef9d253b3df773ccdd53f
Cesare Ferrari <cesare@maryandferrari.com>
2019-08-13 15:12:22 -0400

    Sort steps

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -15,5 +15,9 @@ function findById(id) {
 }
 
 function findSteps(id) {
-  return db('steps').where({scheme_id: id});
+  return db('schemes')
+    .join('steps', 'schemes.id', 'steps.scheme_id')
+    .select('steps.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+    .where({scheme_id: id})
+    .orderBy('steps.step_number');
 }
